b2db63c0e0b20a3686609078df815f8c3bbebdf9
Shinsei <murakami.shinsei.zn@gmail.com>
2023-09-24 16:32:44 +0900

    ポケモンデータ取得

--- pokemon-app/src/App.js
+++ pokemon-app/src/App.js
@@ -1,6 +1,18 @@
+import { useEffect } from "react";
 import "./App.css";
+import { getAllPokemon } from "./utils/pokemon";
 
 function App() {
+  const initialURL = "https://pokeapi.co/api/v2/pokemon";
+
+  useEffect(() => {
+    const fetchPlemonData = async () => {
+      let res = getAllPokemon(initialURL);
+      console.log(res);
+    };
+    fetchPlemonData();
+  }, []);
+
   return <div className="App"></div>;
 }
 
